298ee2ed6f2284c681fe2e4047f7cff7f0728511
Auk4n <maximiliano.2000.dp@gmail.com>
2023-01-23 11:39:12 -0300

    iconos redes sociales

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -40,9 +40,15 @@
             <div class="spanb"></div>Municipalidad La Cruz
           </button>
         </a>
+        <div class="contenedor">
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fab fa-twitter"></i></a>
+                <a href="#"><i class="fab fa-youtube"></i></a>
+              </div>
       </div>
-    </div>
-  </div>
+    </div>   
+</div>
 
 </body>
 
@@ -50,12 +56,27 @@
 @stop
 
 @section('css')
+<link rel="stylesheet" href="resources/css/estilos.css">
 <link rel="stylesheet" href="/css/admin_custom.css">
 <link rel="stylesheet" href="{{ asset('css/stylehome.css') }}">
-
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
+  integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
 @stop
 
 @section('js')
 <script>
+< script src = "https://code.jquery.com/jquery-3.5.1.slim.min.js"
+integrity = "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+crossorigin = "anonymous" >
+</script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+  integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+  integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+</script>
+<script src="https://unpkg.com/bootstrap-table@1.20.2/dist/bootstrap-table.min.js"></script>
+<script src="{{asset('js/bootstrap-table-es-CL.js')}}">
+</script>
 </script>
 @stop
