82eda9495affee74f034ac9af7536817beff788b
benjassjr <benja13bebv@gmail.com>
2023-01-27 11:29:33 -0300

    Nuevo dashboard responsivo

--- resources/views/home/dashboard.blade.php
+++ resources/views/home/dashboard.blade.php
@@ -8,56 +8,104 @@
 
 @section('content')
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
   <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Inicio</title>
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Landing Page HTML | AlexCG Design</title>
 </head>
 
 <body>
-  <div class="titulo banner">
-    <!--Usuario-->
-    <div class="row mr-0 text-right">
-      <div class="col-12">
-        Bienvenido <b>{{Auth::user()->nombre}} ({{Auth::user()->rol->nombre}})</b>
-      </div>
-    </div>
-    <!--Usuario-->
-    <div class="content">
+  <header class="hero">
+    <div class="textos-hero">
       <h1>Seguridad La Cruz</h1>
-      <p>Página web desarrollada por practicantes informáticos</p>
-      <div>
-        <a href="{{route('funcionarios.index')}}">
-          <button type="button">
-            <div class="spanb"></div>Funcionarios
-          </button>
-        </a>
-        <a href="https://www.lacruz.cl">
-          <button type="button">
-            <div class="spanb"></div>Municipalidad La Cruz
-          </button>
-        </a>
-
-      </div>
-      <div class="contenedor">
-        <a href="https://es-la.facebook.com/LACRUZ.CL/">
-          <i class="im fab-facebook fab fa-facebook-f"></i>
-        </a>
-        <a href="https://www.instagram.com/municipalidadlacruz/?hl=es">
-          <i class="im fab-instagram fab fa-instagram"></i>
-        </a>
-        <a href="https://twitter.com/muni_lacruz">
-          <i class="im fab-twitter fab fa-twitter"></i>
-        </a>
-        <a href="https://www.youtube.com/channel/UC9IavHMutzVhCsFN2NGEE6A">
-          <i class="im fab-youtube fab fa-youtube"></i>
-        </a>
-      </div>
+      <p>Bienvenido <b>{{Auth::user()->nombre}}</b></p>
     </div>
-  </div>
+    <div class="svg-hero" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+        style="height: 100%; width: 100%;">
+        <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+          style="stroke: none; fill: #fff;"></path>
+      </svg></div>
+  </header>
+
+
+  <section class="wave-contenedor website">
+    <img src="{{ asset('images/ilustracion1.jpg') }}" alt="">
+    <div class="contenedor-textos-main">
+      <h2 class="titulo left">Municipalidad de la Cruz</h2>
+      <p class="parrafo">La Municipalidad de La Cruz es liderada por la alcaldesa Filomena Navia Hevia y el honorable
+        concejo municipal, quienes ponen su capacidades y disposición al servicio de todos los vecinos y vecinas de esta
+        comuna.</p>
+      <a href="https://www.lacruz.cl" class="cta">Visitar Página Web</a>
+    </div>
+  </section>
+
+  <section class="info">
+    <div class="contenedor">
+      <h2 class="titulo left">Sigamos Avanzando</h2>
+      <p>Ilustre Municipalidad de la Cruz</p>
+    </div>
+  </section>
+
+  <section class="cards contenedor">
+    <h2 class="titulo">Nuestras Redes Sociales</h2>
+    <div class="content-cards">
+      <article class="card">
+        <i class="fab fa-facebook"></i>
+        <h3>Facebook</h3>
+        <p>Contamos con página de facebook.</p>
+        <a href="https://es-la.facebook.com/LACRUZ.CL/" class="cta">Visitar</a>
+      </article>
+      <article class="card">
+        <i class="fab fa-instagram"></i>
+        <h3>Instagram</h3>
+        <p>Por supuesto un perfil de instagram.</p>
+        <a href="https://www.instagram.com/municipalidadlacruz/?hl=es" class="cta">Visitar</a>
+      </article>
+      <article class="card">
+        <i class="fab fa-twitter"></i>
+        <h3>Twitter</h3>
+        <p>Además de cuenta de twitter.</p>
+        <a href="https://twitter.com/muni_lacruz" class="cta">Visitar</a>
+      </article>
+    </div>
+  </section>
+
+  <section class="galeria">
+    <div class="contenedor">
+      <h2 class="titulo">Nuestra Comuna</h2>
+      <article class="galeria-cont">
+        <img src="{{ asset('images/uno.jpg') }}" alt="">
+        <img src="{{ asset('images/dos.jpg') }}" alt="">
+        <img src="{{ asset('images/tres.jpg') }}" alt="">
+        <img src="{{ asset('images/cuatro.jpg') }}" alt="">
+        <img src="{{ asset('images/cinco.jpg') }}" alt="">
+        <img src="{{ asset('images/seis.jpg') }}" alt="">
+      </article>
+    </div>
+  </section>
+
+  <section class="info-last">
+
+    <div class="contenedor last-section">
+      <div class="contenedor-textos-main">
+        <h2 class="titulo left">Participa en nuestra próxima actividad</h2>
+        <p class="parrafo">Este 04 de marzo vivamos todas y todos las 2da. Versión del Festival del Cantar Vecinal La
+          Cruz 2023. </p>
+        <a href="https://www.lacruz.cl" class="cta">Bases e inscripciones aquí</a>
+      </div>
+      <img src="{{ asset('images/ilustracion.jpg') }}" alt="">
+    </div>
+
+    <div class="svg-wave" style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none"
+        style="height: 100%; width: 100%;">
+        <path d="M0.00,49.98 C149.99,150.00 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z"
+          style="stroke: none; fill: #601BB8;"></path>
+      </svg></div>
+  </section>
+
 
 </body>
 
@@ -65,11 +113,9 @@
 @stop
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/estilos.css') }}">
+<link href="https://fonts.googleapis.com/css?family=Raleway:200,300,400,500&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/estilolang.css') }}">
 <link rel="stylesheet" href="/css/admin_custom.css">
-<link rel="stylesheet" href="{{ asset('css/stylehome.css') }}">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-  integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous" />
 @stop
 
 @section('js')
@@ -84,6 +130,7 @@ crossorigin = "anonymous" >
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
   integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
 </script>
+<script src="https://kit.fontawesome.com/c15b744a04.js" crossorigin="anonymous"></script>
 </script>
 </script>
 @stop
